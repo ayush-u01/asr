3e00589e3d25ef098a7b965fe3388c0ff115c0bd
AYUSH UPADHYAY <ayush02u@gmail.com>
2025-07-04 00:23:36 +0530

    min upd

--- app.py
+++ app.py
@@ -47,4 +47,5 @@ def transcribe():
 #     return "Hello from Flask on Render!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
